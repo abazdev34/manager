ddcafc1d86ea2f6a424975826112cc2e055b842d
abazdev34 <abaztoktorbaev89@gmail.com>
2025-05-28 02:13:36 +0300

    fbfbfbfbfbfbfff

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,15 @@
 
 import {Route,  Routes } from 'react-router-dom'
 
-import Header from './components/header/index.jsx'
-import Texkarta from './texkarta/index.jsx'
-import Texkarta2 from './texkarta/kyxTex.jsx'
+
 import TechCard from './texkarta/TechCard.jsx'
 
 function App() {
   return (
  <div className="div">
 
-  <Routes>
 
-  
-    
-
-
- <Route path="/TechCard" element={<TechCard/>} />
-  </Routes>
+  <TechCard/>
 
 
  </div>
